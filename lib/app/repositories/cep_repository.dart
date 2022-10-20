26ecb42608cb6fe612042474297c8c6fef9e6284
Johnathan Rocha <johnathanrocha@gmail.com>
2022-10-20 00:52:09 -0300

    feat: created repositories

---
+++ lib/app/repositories/cep_repository.dart
@@ -0,0 +1,19 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+
+class CepRepository {
+  Future<Map<String, dynamic>?> getAddress(String cep) async {
+    try {
+      final response = await Dio().get("https://viacep.com.br/ws/$cep/json/");
+      if (response.statusCode == 200) {
+        return response.data;
+      } else {
+        return null;
+      }
+    } catch (e, s) {
+      log('Erro ao buscar endereco', error: e, stackTrace: s);
+      throw Exception();
+    }
+  }
+}
