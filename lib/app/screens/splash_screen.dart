eb72d0a5904417df7db851743326a1b91d52a119
Johnathan Rocha <johnathanrocha@gmail.com>
2022-10-19 21:31:48 -0300

    feat: created splash screen ui

--- lib/app/screens/splash_screen.dart
+++ lib/app/screens/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bemol_test/app/core/ui/styles/colors_app.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -6,10 +7,28 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash'),
+      backgroundColor: context.colors.primary,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width * .8,
+              height: MediaQuery.of(context).size.height * .8,
+              child: Image.asset('assets/images/logo_bemol.png'),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * .8,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: context.colors.secondary,
+                ),
+                onPressed: () {},
+                child: const Text('Entrar'),
+              ),
+            )
+          ],
+        ),
       ),
-      body: Container(),
     );
   }
 }
