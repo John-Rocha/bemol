14edadce93616bb2d4563af8df6266446aaf47c8
Johnathan Rocha <johnathanrocha@gmail.com>
2022-10-21 14:30:23 -0300

    feat: adds validators

--- lib/app/screens/form_screen.dart
+++ lib/app/screens/form_screen.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:bemol_test/app/core/ui/styles/colors_app.dart';
 import 'package:bemol_test/app/core/ui/styles/text_styles.dart';
+import 'package:bemol_test/app/core/utils/input_formatter.dart';
 import 'package:flutter/material.dart';
+import 'package:validatorless/validatorless.dart';
 
 class FormScreen extends StatefulWidget {
   const FormScreen({super.key});
@@ -13,8 +15,25 @@ class FormScreen extends StatefulWidget {
 class _FormScreenState extends State<FormScreen> {
   final _formKey = GlobalKey<FormState>();
 
-  String _genderValue = 'sexo';
-  List<String> genres = ['sexo', 'homem', 'mulher', 'trans', 'outro'];
+  // Person controllers
+  final _nameEC = TextEditingController();
+  final _lastNameEC = TextEditingController();
+  final _birthEC = TextEditingController();
+  String _genderValue = 'Sexo';
+  final _cpfEC = TextEditingController();
+  final _rgEC = TextEditingController();
+  final _emailEC = TextEditingController();
+  final _phoneEC = TextEditingController();
+
+  // Address controllers
+  final _cepEC = TextEditingController();
+  final _logradouroEC = TextEditingController();
+  final _numberEC = TextEditingController();
+  final _bairroEC = TextEditingController();
+  final _cidadeEC = TextEditingController();
+  final _estadoEC = TextEditingController();
+
+  List<String> genres = ['Sexo', 'Homem', 'Mulher', 'Trans', 'Outro'];
 
   @override
   Widget build(BuildContext context) {
@@ -36,212 +55,256 @@ class _FormScreenState extends State<FormScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
+                _personData(),
                 const SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Dados Pessoais',
-                      style: context.textStyles.titleWhite,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                ListView(
-                  shrinkWrap: true,
-                  children: [
-                    Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Expanded(
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              floatingLabelBehavior:
-                                  FloatingLabelBehavior.never,
-                              labelText: 'Nome',
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        Expanded(
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              floatingLabelBehavior:
-                                  FloatingLabelBehavior.never,
-                              labelText: 'Sobrenome',
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              floatingLabelBehavior:
-                                  FloatingLabelBehavior.never,
-                              labelText: 'Nascimento',
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        Expanded(
-                          child: _gender(),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              floatingLabelBehavior:
-                                  FloatingLabelBehavior.never,
-                              labelText: 'CPF',
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        Expanded(
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              floatingLabelBehavior:
-                                  FloatingLabelBehavior.never,
-                              labelText: 'RG',
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              floatingLabelBehavior:
-                                  FloatingLabelBehavior.never,
-                              labelText: 'Email',
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        Expanded(
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              floatingLabelBehavior:
-                                  FloatingLabelBehavior.never,
-                              labelText: 'Telefone',
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Endereço',
-                          style: context.textStyles.titleWhite,
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              floatingLabelBehavior:
-                                  FloatingLabelBehavior.never,
-                              labelText: 'CEP',
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: context.colors.secondary,
-                          ),
-                          onPressed: () {},
-                          child: const Text('Buscar'),
-                        )
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        floatingLabelBehavior: FloatingLabelBehavior.never,
-                        labelText: 'Endereço',
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              floatingLabelBehavior:
-                                  FloatingLabelBehavior.never,
-                              labelText: 'Número',
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        Expanded(
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              floatingLabelBehavior:
-                                  FloatingLabelBehavior.never,
-                              labelText: 'Bairro',
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              floatingLabelBehavior:
-                                  FloatingLabelBehavior.never,
-                              labelText: 'Cidade',
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        Expanded(
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              floatingLabelBehavior:
-                                  FloatingLabelBehavior.never,
-                              labelText: 'Estado',
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                _addressData(),
                 const SizedBox(height: 10),
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
                     backgroundColor: context.colors.secondary,
                   ),
-                  onPressed: () {},
+                  onPressed: () {
+                    final isValid = _formKey.currentState?.validate() ?? false;
+                    if (!isValid) {
+                      return;
+                    }
+                  },
                   child: const Text('Enviar'),
-                )
+                ),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget _personData() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Dados Pessoais',
+              style: context.textStyles.titleWhite,
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Expanded(
+              child: TextFormField(
+                controller: _nameEC,
+                decoration: const InputDecoration(
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  labelText: 'Nome',
+                ),
+                validator: Validatorless.required('Campo obrigatório'),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: TextFormField(
+                controller: _lastNameEC,
+                decoration: const InputDecoration(
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  labelText: 'Sobrenome',
+                ),
+                validator: Validatorless.required('Campo obrigatório'),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
+        Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                controller: _birthEC,
+                inputFormatters: [
+                  InputFormatter.instance.birthDateFormatter,
+                ],
+                decoration: const InputDecoration(
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  labelText: 'Nascimento',
+                ),
+                validator: Validatorless.required('Campo obrigatório'),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: _gender(),
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
+        Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                controller: _cpfEC,
+                inputFormatters: [
+                  InputFormatter.instance.cpfFormatter,
+                ],
+                decoration: const InputDecoration(
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  labelText: 'CPF',
+                ),
+                validator: Validatorless.required('Campo obrigatório'),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: TextFormField(
+                controller: _rgEC,
+                decoration: const InputDecoration(
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  labelText: 'RG',
+                ),
+                validator: Validatorless.required('Campo obrigatório'),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
+        Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                controller: _emailEC,
+                decoration: const InputDecoration(
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  labelText: 'Email',
+                ),
+                validator: Validatorless.multiple([
+                  Validatorless.required('Obrigatório'),
+                  Validatorless.email('E-mail inválido'),
+                ]),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: TextFormField(
+                controller: _phoneEC,
+                inputFormatters: [
+                  InputFormatter.instance.phoneFormatter,
+                ],
+                decoration: const InputDecoration(
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  labelText: 'Telefone',
+                ),
+                validator: Validatorless.required('Campo obrigatório'),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _addressData() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Endereço',
+              style: context.textStyles.titleWhite,
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
+        Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                inputFormatters: [
+                  InputFormatter.instance.cepFormatter,
+                ],
+                controller: _cepEC,
+                decoration: const InputDecoration(
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  labelText: 'CEP',
+                ),
+                validator: Validatorless.required('Campo obrigatório'),
+              ),
+            ),
+            const SizedBox(width: 10),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: context.colors.secondary,
+              ),
+              onPressed: () {},
+              child: const Text('Buscar'),
+            )
+          ],
+        ),
+        const SizedBox(height: 10),
+        TextFormField(
+          controller: _logradouroEC,
+          decoration: const InputDecoration(
+            floatingLabelBehavior: FloatingLabelBehavior.never,
+            labelText: 'Endereço',
+          ),
+          validator: Validatorless.required('Campo obrigatório'),
+        ),
+        const SizedBox(height: 10),
+        Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                controller: _numberEC,
+                decoration: const InputDecoration(
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  labelText: 'Número',
+                ),
+                validator: Validatorless.required('Campo obrigatório'),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: TextFormField(
+                controller: _bairroEC,
+                decoration: const InputDecoration(
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  labelText: 'Bairro',
+                ),
+                validator: Validatorless.required('Campo obrigatório'),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
+        Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                controller: _cidadeEC,
+                decoration: const InputDecoration(
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  labelText: 'Cidade',
+                ),
+                validator: Validatorless.required('Campo obrigatório'),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: TextFormField(
+                controller: _estadoEC,
+                decoration: const InputDecoration(
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  labelText: 'Estado',
+                ),
+                validator: Validatorless.required('Campo obrigatório'),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 
